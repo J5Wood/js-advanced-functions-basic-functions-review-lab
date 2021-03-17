6dad742d8d2cfd7b3f9b95b7ee989dd3176a3aaf
Jeremy Wood <js.wood206@gmail.com>
2021-03-16 21:31:08 -0700

    commit tree dd758d86b309d31666e1114b11448908df49961d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+}
+
+function mondayWork(activity = "go to the office") {
+    return "This Monday, I will " + activity + ".";
+}
+
+function wrapAdjective(outerParam = "*") {
+    return function(innerParam = "Special") {
+        return "You are " + outerParam + innerParam + outerParam + "!";
+    }
+}
+
+const Calculator = {
+    add: () =>  1 + 3,
+    subtract: () =>  1 - 3,
+    multiply: () =>  1 * 3,
+    divide: () =>  10 / 5
+}
+
+function actionApplyer(start, functions) {
+    functions.map(function(func) {
+        start = func(start)
+    })
+    return start;
+}
+
